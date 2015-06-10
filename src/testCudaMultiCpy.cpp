60108499237b2c996300c3e7cee1b75027384557
maw170 <maw170@case.edu>
2015-06-10 08:34:17 -0400

    Additional Test within Multi Copy
    Added a second test to testCudaMultiCpy.cpp.  This test is represented
    by "TIME 3: " and attempts to see if passing a large object followed by
    returning a smaller object is more efficient then passing a large object
    and returning it.  The current results indicated that yes, passing a
    large object and returning a smaller object is faster.  This occurs
    despite the need to run additional cudaMalloc commands.

--- src/testCudaMultiCpy.cpp
+++ src/testCudaMultiCpy.cpp
@@ -12,6 +12,9 @@
 //RESULT:
 //TIME 1 = 343.766
 //TIME 2 = 0.2000
+//Review of test shows that first call actually takes a significantly
+//longer time to complete than subsiquent calls.  Will have to look
+//into this further to determine cause.
 #include <stdio.h>
 #include <cuda.h>
 #include <time.h>
@@ -26,6 +29,7 @@ int main(){
 	int col2 [10000];
 	int col3 [10000];
 	int arr [10000][3];
+	int sub [10000][2];
 
 	for (int i = 0; i<10000; i++){
 		col1[i] = 10;
@@ -33,13 +37,16 @@ int main(){
 		col3[i] = 42;
 		for (int j = 0; j<3; j++)
 			arr[i][j] = i+j;
+		for (int j = 0; j<2; j++)
+			sub[i][j] = i+j;
 		printf("%d %d %d %d\n", col1[i], col2[i], col3[i], arr[i][2]);
 	}
 
 	size_t size1 = sizeof(int) * 10000;
 	size_t size2 = sizeof(int) * 10000 * 3;
+	size_t size3 = sizeof(int) * 10000 * 2;
 
-	int *pcol1, *pcol2, *pcol3, *parr;
+	int *pcol1, *pcol2, *pcol3, *parr, *psub;
 
 	clock_t start;
 	double duration;
@@ -48,20 +55,38 @@ int main(){
 	printf("START ALLOCATION\n");
 	start = clock();
 	cudaMalloc(&pcol1, size1);
+	cudaFree(pcol1);
+	duration = (clock() - start)/(double) CLOCKS_PER_SEC * 1000;
+	
+	start = clock();
+	cudaMalloc(&pcol1, size1);
 	cudaMalloc(&pcol2, size1);
+	//duration = (clock() - start)/(double) CLOCKS_PER_SEC * 1000;
 	cudaMalloc(&pcol3, size1);
 	cudaMemcpy(pcol1, &col1, size1, cudaMemcpyHostToDevice);
 	cudaMemcpy(pcol2, &col2, size1, cudaMemcpyHostToDevice);
 	cudaMemcpy(pcol3, &col3, size1, cudaMemcpyHostToDevice);
 	duration = (clock() - start)/(double) CLOCKS_PER_SEC * 1000;
-
 	printf("TIME 1: %f\n", duration);
 
 	start = clock();
 	cudaMalloc(&parr, size2);
 	cudaMemcpy(parr, &arr, size2, cudaMemcpyHostToDevice);
+	cudaMemcpy(arr, parr, size2, cudaMemcpyDeviceToHost);
 	duration = (clock() - start)/(double) CLOCKS_PER_SEC * 1000;
 
 	printf("TIME 2: %f\n", duration);
+
+
+	cudaFree(pcol1);
+	start = clock();
+	cudaMalloc(&pcol1, size1);
+	cudaMalloc(&psub, size3);
+	cudaMemcpy(psub, &sub, size3, cudaMemcpyHostToDevice);
+	
+	cudaMemcpy(col1, pcol1, size1, cudaMemcpyDeviceToHost);
+	duration = (clock() - start)/(double) CLOCKS_PER_SEC * 1000;
+	printf("TIME 3: %f\n", duration);
+
 	return 0;
 }
